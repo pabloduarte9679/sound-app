a016c61375082c4a8424b2426bcc13495d677c3c
pablo <92185570+pabloduarte9679@users.noreply.github.com>
2026-02-20 15:16:05 -0600

    Create mockData.js

---
+++ src/data/mockData.js
@@ -0,0 +1,14 @@
+export const SONGS = [
+  { id: '1', title: 'Bohemian Rhapsody', artist: 'Queen', cover: 'https://picsum.photos/100/100' },
+  { id: '2', title: 'Billie Jean', artist: 'Michael Jackson', cover: 'https://picsum.photos/101/101' },
+
+  { id: '3', title: 'Shape of You', artist: 'Ed Sheeran', cover: 'https://picsum.photos/102/102' },
+  { id: '4', title: 'Rolling in the Deep', artist: 'Adele', cover: 'https://picsum.photos/103/103' },
+  { id: '5', title: 'Smells Like Teen Spirit', artist: 'Nirvana', cover: 'https://picsum.photos/104/104' },
+  { id: '6', title: 'Blinding Lights', artist: 'The Weeknd', cover: 'https://picsum.photos/105/105' },
+  { id: '7', title: 'Hotel California', artist: 'Eagles', cover: 'https://picsum.photos/106/106' },
+  { id: '8', title: 'Sweet Child O’ Mine', artist: 'Guns N’ Roses', cover: 'https://picsum.photos/107/107' },
+  { id: '9', title: 'Imagine', artist: 'John Lennon', cover: 'https://picsum.photos/108/108' },
+  { id: '10', title: 'Uptown Funk', artist: 'Mark Ronson ft. Bruno Mars', cover: 'https://picsum.photos/109/109' },
+];
+
